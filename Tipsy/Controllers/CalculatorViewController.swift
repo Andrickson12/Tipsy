9547defbdb14eaef11cd238e07440bd5c9e55f0c
Andrickson12 <Andrickson12>
2020-02-09 20:05:11 -0500

    fdgnb

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+    //Outlets
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
+    @IBOutlet weak var warningLabel: UILabel!
     
-    var tip = 0.0
+    var tip = 0.10
+    var numberOfPeople = 2
+    var billTotal = 0.0
     
     @IBAction func tipChanged(_ sender: UIButton) {
         //Deselect all tip buttons via IBOutlets
@@ -39,15 +43,40 @@ class CalculatorViewController: UIViewController {
         //Divide the percent expressed out of 100 into a decimal e.g. 10 becomes 0.1
         tip = buttonTitleAsANumber / 100
         
+        billTextField.endEditing(true)
+        
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        splitNumberLabel.text = String(Int(sender.value))
+        
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
         
-        print(tip)
+        //Get the text the user typed in the billTextField
+        let bill = billTextField.text!
+        
+        if bill != "" {
+            
+            //Turn the bill from a String e.g. "123.50" to an actual String with decimal places.
+            //e.g. 125.50
+            billTotal = Double(bill)!
+            
+            //Multiply the bill by the tip percentage and divide by the number of people to split the bill.
+            let result = billTotal * (1 + tip) / Double(numberOfPeople)
+            
+            //Round the result to 2 decimal places and turn it into a String.
+            let resultTo2DecimalPlaces = String(format: "%.2f", result)
+            
+            print(resultTo2DecimalPlaces)
+            
+            warningLabel.text = resultTo2DecimalPlaces
+        }
+        
     }
+    
     
 }
 
